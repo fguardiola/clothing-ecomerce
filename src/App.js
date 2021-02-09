30474cfde0f42ce8d71275f499bae80651a12603
fguardiola <fernando.guardiola.888@gmail.com>
2021-02-09 20:55:13 +0100

    implement basic homepage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
 
+import './homepage.styles.scss';
+
+import HomePage from './homepage.component';
+
 function App() {
   return (
     <div className="App">
-      Hello World!
+      <HomePage />
     </div>
   );
 }
